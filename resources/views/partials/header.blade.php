28336911265b142e9a348e7199cacedb9933c1f9
RickCraco <riccardocracolici43@gmail.com>
2023-12-18 14:23:00 +0100

    aggiunto menu header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,17 @@
 <header>
-    Header <i class="fa-solid fa-house"></i>
+    <div class="container d-flex align-items-center justify-content-between py-3">
+        <img src="../../public/img/dc-logo.png" alt="logo">
+        <ul class="d-flex nav">
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">CHARACTERS</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">COMICS</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">MOVIES</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">TV</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">GAMES</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">COLLECTIBLES</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">VIDEOS</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">FANS</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">NEWS</a></li>
+            <li class="px-3 nav-item" ><a href="#" class="nav-link">SHOP</a></li>
+        </ul>
+    </div>
 </header>
