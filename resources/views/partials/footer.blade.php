0875382023600792a8101b52851c8526a26bc461
RickCraco <riccardocracolici43@gmail.com>
2023-12-18 14:42:03 +0100

    aggiunto layout footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,59 @@
 <footer>
-    Footer 
+    <div class="container">
+        <div class="row">
+
+            <div class="col-6 py-3 d-flex my-5">
+                <div>
+                    <ul class="">
+                        <li><h4 class="text-white">Dc Comics</h4></li>
+                        <li class="text-white nav-item">DC</li>
+                        <li class="text-white nav-item">DC Universe</li>
+                        <li class="text-white nav-item">DC Power Visa</li>
+                        <li class="text-white nav-item">Shop DC</li>
+                        <li class="text-white nav-item">Shop DC Collectibles</li>
+                    </ul>
+
+                    <ul>
+                        <li><h4 class="text-white">SHOP</h4></li>
+                        <li class="text-white nav-item">Shop</li>
+                        <li class="text-white nav-item">Shop DC</li>
+                        <li class="text-white nav-item">Shop DC Collectibles</li>
+                    </ul>
+                </div>
+                    
+                <div>
+                    <ul>
+                        <li><h4 class="text-white">DC</h4></li>
+                        <li class="text-white nav-item">Term Of Use</li>
+                        <li class="text-white nav-item">Privacy policy (New)</li>
+                        <li class="text-white nav-item">Ad Choices</li>
+                        <li class="text-white nav-item">Advertising</li>
+                        <li class="text-white nav-item">Jobs</li>
+                        <li class="text-white nav-item">Subscriptions</li>
+                        <li class="text-white nav-item">Talent Workshops</li>
+                        <li class="text-white nav-item">CPSC Certificates</li>
+                        <li class="text-white nav-item">Ratings</li>
+                        <li class="text-white nav-item">Shop Help</li>
+                        <li class="text-white nav-item">Contact Us</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <ul>
+                        <li><h4 class="text-white">SITES</h4></li>
+                        <li class="text-white nav-item">DC</li>
+                        <li class="text-white nav-item">MAD Magazine</li>
+                        <li class="text-white nav-item">DC Kids</li>
+                        <li class="text-white nav-item">DC Universe</li>
+                        <li class="text-white nav-item">DC Power Visa</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="col-6 position-relative">
+                <img src="img/dc-logo-bg.png" alt="">
+            </div>
+
+        </div>
+    </div>
 </footer>
