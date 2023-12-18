68f1fd10638421446c64c6c035e7a5bcc50b91b9
RickCraco <riccardocracolici43@gmail.com>
2023-12-18 14:34:55 +0100

    aggiunto main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,28 @@
 @section('title', 'Home')
 
 @section('content')
-    <main>
-        <h1>Home Page</h1>
-    </main>
+<nav>
+    <div class="py-4 text-white boack">
+        <section class="w-100 sezione-top position-relative">
+            <h3 class="bg-primary text-white text-uppercase p-3">Current series</h3>
+        </section>
+            <section>
+            <div class="container py-5">
+                <div class="row">
+                    
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="my-bg" id="div-blu ">
+        <div class="container">
+            <div class="d-flex nav align-items-center justify-content-center">
+                <div class="px-3 nav-item d-flex align-items-center py-4">
+                    <div><img  alt=""></div>
+                    <span class="px-2 text-white"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
 @endsection
