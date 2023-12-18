7b5db275bdfc58583b2bbc6e414a1a8b7cd59949
RickCraco <riccardocracolici43@gmail.com>
2023-12-18 14:51:43 +0100

    prova aggiunta immagini

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container d-flex align-items-center justify-content-between py-3">
-        <img src="img/dc-logo.png" alt="logo">
+        <img src="{{Vite::asset('resources/img/dc-logo.png')}}" alt="logo">
         <ul class="d-flex nav">
             <li class="px-3 nav-item" ><a href="#" class="nav-link">CHARACTERS</a></li>
             <li class="px-3 nav-item" ><a href="#" class="nav-link">COMICS</a></li>
